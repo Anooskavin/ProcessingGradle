e037eda6aaf3d62bf5cc252187f235e8a9a787cf
Anooskavin <53963293+Anooskavin@users.noreply.github.com>
2023-12-27 22:38:08 +0530

    Cleared SDK Issue

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,9 @@
 package org.example;
 
-public class Main  {
+
+import processing.core.PApplet;
+
+public class Main  extends PApplet {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
