172b680f59db603e3c0f63e1ad998806ed25ecbd
Anooskavin <53963293+Anooskavin@users.noreply.github.com>
2023-12-27 22:25:53 +0530

    Changed Structure

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+import processing.core.PApplet;
+
+public class Main extends PApplet {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
